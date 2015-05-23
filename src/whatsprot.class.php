42a55ba60db1f80e93f1c23fb2f8aef428f5a71d
mgp25 <me@mgp25.com>
2015-05-23 10:19:56 +0200

    fix glitch lastseen

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -3010,6 +3010,7 @@ class WhatsProt
                             $this->phoneNumber,
                             $blockedJids
                         ));
+                    return;
                 }
                 $this->eventManager()->fire("onGetRequestLastSeen",
                     array(
@@ -3018,7 +3019,6 @@ class WhatsProt
                         $node->getAttribute('id'),
                         $node->getChild(0)->getAttribute('seconds')
                     ));
-                array_push($this->messageQueue, $node);
             }
             if ($node->getChild("props") != null) {
                 //server properties
